f1697147b3bcddf864a14dd661498382b7bd4fba
Todd Stevens <scarfireweb@gmail.com>
2020-06-04 22:19:20 -0400

    Added ShowViewController toolbar, buttons and stubbed out actions

---
+++ DC Shows iPhone/View Controllers/Show Controllers/ShowViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ShowViewController.swift
+//  DC Shows iPhone
+//
+//  Created by Todd Stevens on 6/4/20.
+//  Copyright © 2020 Todd Stevens. All rights reserved.
+//
+
+import UIKit
+
+class ShowViewController: UIViewController {
+
+    @IBAction func edit(_ sender: Any) {
+    }
+
+    @IBAction func photos(_ sender: Any) {
+    }
+    
+    @IBAction func videos(_ sender: Any) {
+    }
+    
+    @IBAction func audio(_ sender: Any) {
+    }
+    
+    @IBAction func random(_ sender: Any) {
+    }
+    
+}
