a69840d96185dd9a51b4fe2d6d501377280322b4
Todd Stevens <scarfireweb@gmail.com>
2020-06-06 16:16:09 -0400

    Recreated ToursViewController and renamed models

--- DC Shows iPhone/View Controllers/Show Controllers/ToursViewController.swift
+++ DC Shows iPhone/View Controllers/Show Controllers/ToursViewController.swift
@@ -2,31 +2,26 @@
 //  ToursViewController.swift
 //  DC Shows iPhone
 //
-//  Created by Todd Stevens on 6/5/20.
+//  Created by Todd Stevens on 6/6/20.
 //  Copyright © 2020 Todd Stevens. All rights reserved.
 //
 
 import UIKit
 
 class ToursViewController: UIViewController, TourModelProtocol {
-    var tours: [TourModel] = []
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
+        var tours: [TourModel] = []
+        
+        override func viewDidLoad() {
+            super.viewDidLoad()
 
-        let tourModel = TourModel()
-        tourModel.delegate = self
-        tourModel.downloadItems()
-//        let tour = tourItems[0] as! [String:Any]
-//        print(tour["year"] as! Int)
-//        print(tour["poster"] as! String)
-    }
+            let tourModel = TourModel()
+            tourModel.delegate = self
+            tourModel.downloadItems()
+        }
 
-    func itemsDownloaded(items: [TourModel]) {
-            tours = items
-//            let tour = tours[2]
-//            print(tour.poster!)
-    }
-
+        func itemsDownloaded(items: [TourModel]) {
+                tours = items
+    //            let tour = tours[2]
+    //            print(tour.poster!)
+        }
 }
-
