f4a47943e5f0e7ae330900d9a05b1d44b8a68939
Todd Stevens <scarfireweb@gmail.com>
2020-06-06 19:14:14 -0400

    Added Show Collection View Cell class

---
+++ DC Shows iPhone/Custom Controls/ShowCollectionViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  ShowCollectionViewCell.swift
+//  DC Shows iPhone
+//
+//  Created by Todd Stevens on 6/6/20.
+//  Copyright © 2020 Todd Stevens. All rights reserved.
+//
+
+import UIKit
+
+class ShowCollectionViewCell: UICollectionViewCell {
+    @IBOutlet weak var poster: UIImageView!
+    @IBOutlet weak var lblDate: UILabel!
+    @IBOutlet weak var lblLocation: UILabel!
+    
+}
