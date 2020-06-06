2820181bf801d0027b8a6f9aa61ace5f02f2aa30
Todd Stevens <scarfireweb@gmail.com>
2020-06-06 18:27:00 -0400

    Added ShowModel ver 1

--- DC Shows iPhone/DataModels/ShowModel.swift
+++ DC Shows iPhone/DataModels/ShowModel.swift
@@ -8,6 +8,35 @@
 
 import Foundation
 
-class ShowModel: NSObject {
+protocol ShowModelProtocol: class {
+    func itemsDownloaded(items: [ShowModel])
+}
 
+class ShowModel: NSObject {
+    var year: Int = 0
+    var poster: String? = ""
+
+    weak var delegate: ShowModelProtocol!
+
+    func downloadItems() {
+        let url = URL(string: "https://toddlstevens.com/apps/dcshows/mobile/server/getshowsfortour.php?year=\(year)")
+        let data = try? Data(contentsOf: url!)
+        let jsonResult = try! JSONSerialization.jsonObject(with: data!, options: JSONSerialization.ReadingOptions.mutableContainers) as! NSArray
+
+        var jsonElement = NSDictionary()
+        var tours = [TourModel]()
+        for i in 0 ..< jsonResult.count {
+            jsonElement = jsonResult[i] as! NSDictionary
+            let tour = TourModel()
+            if let year = jsonElement["year"] as? Int,
+                let poster = jsonElement["poster"] as? String {
+                tour.year = year
+                tour.poster = "https://toddlstevens.com/apps/dcshows/images/posters/\(poster)"
+            }
+            tours.append(tour)
+        }
+        DispatchQueue.main.async(execute: { () -> Void in
+            self.delegate.itemsDownloaded(items: tours)
+        })
+    }
 }
