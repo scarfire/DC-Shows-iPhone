2256bdfa2e874305eaad2c982ad4c79a34283b55
Todd Stevens <scarfireweb@gmail.com>
2020-06-19 19:56:36 -0400

    Removed commented code

--- DC Shows iPhone/View Controllers/Show Controllers/VideosViewController.swift
+++ DC Shows iPhone/View Controllers/Show Controllers/VideosViewController.swift
@@ -52,23 +52,6 @@ class VideosViewController: UIViewController {
          vc.showID = showID
          navigationController?.pushViewController(vc, animated: true)
      }
-    
-    // Delete a row from video list
-//    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
-//          if editingStyle == .delete {
-//               // Delete the row from the server
-//               let walkingEvent = values[indexPath.row] as! [String:Any]
-//               let eventID = (walkingEvent["id"] as! Int)
-//               let url = "https://toddlstevens.com/apps/walking/deleteevent.php"
-//               let postString = "&id=" + String(describing: eventID)
-//               let request = NSMutableURLRequest(url: URL(string: url)!)
-//               request.httpMethod = "POST"
-//               request.httpBody = postString.data(using: String.Encoding.utf8)
-//               updateDatabase(request as URLRequest)
-//
-//           } else if editingStyle == .insert {
-//               // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-//           }
 
 }
 
