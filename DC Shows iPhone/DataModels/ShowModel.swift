1bd0954a3f4e27d9eeabada2e087b45f4f4b3f03
Todd Stevens <scarfireweb@gmail.com>
2020-06-21 23:18:28 -0400

    Got search to return matching shows

--- DC Shows iPhone/DataModels/ShowModel.swift
+++ DC Shows iPhone/DataModels/ShowModel.swift
@@ -18,6 +18,8 @@ class ShowModel: NSObject {
     var location: String? = ""
     var showDate: String? = ""
     var poster: String? = ""
+    var showIDs = [Int]()
+    var shows = [ShowModel]()
 
     weak var delegate: ShowModelProtocol!
 
@@ -36,7 +38,6 @@ class ShowModel: NSObject {
         let jsonResult = try! JSONSerialization.jsonObject(with: data!, options: JSONSerialization.ReadingOptions.mutableContainers) as! NSArray
         
         var jsonElement = NSDictionary()
-        var shows = [ShowModel]()
         for i in 0 ..< jsonResult.count {
             jsonElement = jsonResult[i] as! NSDictionary
             let show = ShowModel()
@@ -52,52 +53,22 @@ class ShowModel: NSObject {
             shows.append(show)
         }
         DispatchQueue.main.async(execute: { () -> Void in
-            self.delegate.itemsDownloaded(items: shows)
+            self.delegate.itemsDownloaded(items: self.shows)
         })
-    }
-    
-    func search(searchStr: String, serverDataSource: String) {
-        if serverDataSource == "PHP" {
-            searchPHP(searchStr)
-        }
     }
 
-    fileprivate func downloadFromPHP(year: String) {
-        // Get shows for tour year
-        let url = URL(string: "https://toddlstevens.com/apps/dcshows/mobile/server/getshowsfortour.php?year=\(year)")
-        let data = try? Data(contentsOf: url!)
-        let jsonResult = try! JSONSerialization.jsonObject(with: data!, options: JSONSerialization.ReadingOptions.mutableContainers) as! NSArray
-        
-        var jsonElement = NSDictionary()
-        var shows = [ShowModel]()
-        for i in 0 ..< jsonResult.count {
-            jsonElement = jsonResult[i] as! NSDictionary
-            let show = ShowModel()
-            if let id = jsonElement["id"] as? Int,
-                let location = jsonElement["city_state_country"] as? String,
-                let showDate = jsonElement["showdate"] as? String,
-                let poster = jsonElement["poster"] as? String {
-                show.id = id
-                show.showDate = showDate
-                show.location = location
-                show.poster = "https://toddlstevens.com/apps/dcshows/images/posters/\(poster)"
-            }
-            shows.append(show)
-        }
-        DispatchQueue.main.async(execute: { () -> Void in
-            self.delegate.itemsDownloaded(items: shows)
-        })
+    fileprivate func searchFireBase(searchStr: String) {
+        getShowsForSearchFromFireBase(searchStr: searchStr)
     }
-    
-    func downloadFromFireBase(year: String) {
-        let intYear = Int32(year)
+
+    func getShowForFireBase(showID: Int) {
         let db = getDBReference()
-        db.collection("shows").order(by: "year", descending: true).getDocuments() { (querySnapshot, error) in
-            guard let documents = querySnapshot?.documents else {
-                print("Error fetching documents: \(error!)")
-                return
-            }
-            var shows = [ShowModel]()
+        db.collection("shows").whereField("show_ID", isEqualTo: showID).getDocuments() { (querySnapshot, error) in
+           guard let documents = querySnapshot?.documents else {
+               print("Error fetching documents: \(error!)")
+               return
+           }
+
             for doc in documents {
                 let show = ShowModel()
                 if let showDate = doc.data()["showdate"] as? String,
@@ -108,10 +79,102 @@ class ShowModel: NSObject {
                     show.location = location
                     show.poster = "https://toddlstevens.com/apps/dcshows/images/posters/\(poster)"
                 }
-                shows.append(show)
+                self.shows.append(show)
+                if showID == self.showIDs[self.showIDs.count-1] {
+                    // Done getting all shows - display
+                    DispatchQueue.main.async(execute: { () -> Void in
+                        self.delegate.itemsDownloaded(items: self.shows)
+                    })
+                }
+            }
+        }
+    }
+
+    func getShowsForSearchFromFireBase(searchStr: String) {
+        // Get shows where searched song exists
+        let db = getDBReference()
+        // Find shows with selected song
+        db.collection("set_lists").whereField("title", isEqualTo: "Terrapin Station") .getDocuments() { (querySnapshot, error) in
+            guard let documents = querySnapshot?.documents else {
+                print("Error fetching documents: \(error!)")
+                return
+            }
+            for doc in documents {
+                if let showID = doc.data()["show_id"] as? Int
+                {
+                    self.showIDs.append(showID)
+                }
+            }
+            if self.showIDs.count > 0 {
+                DispatchQueue.main.async(execute: { () -> Void in
+                    self.searchedShowsMatched()
+                })
+            }
+        }
+    }
+    
+    func searchedShowsMatched() {
+        for showID in showIDs {
+            getShowForFireBase(showID: showID)
+        }
+    }
+    
+    func search(searchStr: String, serverDataSource: String) {
+        if serverDataSource == "PHP" {
+            searchPHP(searchStr)
+        }
+        else {
+            searchFireBase(searchStr: searchStr)
+        }
+    }
+
+    fileprivate func downloadFromPHP(year: String) {
+        // Get shows for tour year
+        let url = URL(string: "https://toddlstevens.com/apps/dcshows/mobile/server/getshowsfortour.php?year=\(year)")
+        let data = try? Data(contentsOf: url!)
+        let jsonResult = try! JSONSerialization.jsonObject(with: data!, options: JSONSerialization.ReadingOptions.mutableContainers) as! NSArray
+        
+        var jsonElement = NSDictionary()
+        for i in 0 ..< jsonResult.count {
+            jsonElement = jsonResult[i] as! NSDictionary
+            let show = ShowModel()
+            if let id = jsonElement["id"] as? Int,
+                let location = jsonElement["city_state_country"] as? String,
+                let showDate = jsonElement["showdate"] as? String,
+                let poster = jsonElement["poster"] as? String {
+                show.id = id
+                show.showDate = showDate
+                show.location = location
+                show.poster = "https://toddlstevens.com/apps/dcshows/images/posters/\(poster)"
+            }
+            self.shows.append(show)
+        }
+        DispatchQueue.main.async(execute: { () -> Void in
+            self.delegate.itemsDownloaded(items: self.shows)
+        })
+    }
+    
+    func downloadFromFireBase(year: String) {
+        let db = getDBReference()
+        db.collection("shows").order(by: "year", descending: true).getDocuments() { (querySnapshot, error) in
+            guard let documents = querySnapshot?.documents else {
+                print("Error fetching documents: \(error!)")
+                return
+            }
+            for doc in documents {
+                let show = ShowModel()
+                if let showDate = doc.data()["showdate"] as? String,
+                    let poster = doc.data()["poster"] as? String,
+                    let location = doc.data()["city_state_country"] as? String
+                {
+                    show.showDate = showDate
+                    show.location = location
+                    show.poster = "https://toddlstevens.com/apps/dcshows/images/posters/\(poster)"
+                }
+                self.shows.append(show)
             }
             DispatchQueue.main.async(execute: { () -> Void in
-                self.delegate.itemsDownloaded(items: shows)
+                self.delegate.itemsDownloaded(items: self.shows)
             })
         }
     }
