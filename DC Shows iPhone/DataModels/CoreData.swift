d998393638260188390cce1c16d69c0b06955b27
Todd Stevens <scarfireweb@gmail.com>
2020-06-24 10:31:14 -0400

    Added PHP setlist and show retrieval for all shows to store in Core Data

---
+++ DC Shows iPhone/DataModels/CoreData.swift
@@ -0,0 +1,15 @@
+//
+//  CoreData.swift
+//  DC Shows iPhone
+//
+//  Created by Todd Stevens on 6/24/20.
+//  Copyright © 2020 Todd Stevens. All rights reserved.
+//
+// This model is used to pull shows and set lists data from MySQL Server and into local Core Data file
+
+import Foundation
+import CoreData
+
+class CoreData {
+    
+}
